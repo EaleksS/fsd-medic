008f6a8bb4270256c52892dc75a7b77f8ef42e27
EaleksS <ealgenv@gmail.com>
2023-05-19 20:08:29 +0900

    .

--- src/widgets/components/NavBar/NavBar.tsx
+++ src/widgets/components/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useMemo, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import styles from "./NavBar.module.scss";
 import { Extra } from "../../../entities";
 import { Blur } from "../../../shared";
